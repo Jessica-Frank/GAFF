579db3038b27f396f4b72221d5a4fb8a9d8ececd
Jessica Frank <J_Frank@uncg.edu>
2022-10-15 20:35:54 -0400

    Fixed player page links, added moderator's edit page, added details to game addition confirmaion, and added demo autofill for game addition form

--- src/main/java/com/gaff/demo/ModController.java
+++ src/main/java/com/gaff/demo/ModController.java
@@ -2,7 +2,7 @@ package com.gaff.demo;
 
 /*
  * This controller is for moderator-specific sections of the site.
- * Last updated 10/9/2022
+ * Last updated 10/15/2022
  * Author(s): Jessica Frank
  */
 
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ModController {    
@@ -19,12 +20,40 @@ public class ModController {
     }
     
     @PostMapping("/add_game/details")
-    public String addGameConfirm(Model model) {
+    public String addGameConfirm(Model model, @RequestParam String gameName, 
+            @RequestParam String apiKey) { 
+        model.addAttribute("nameEntered", gameName); 
+        
+        //When the API is implemented, the data below will be from the API
+        model.addAttribute("actualName", gameName);
+        String genre = "puzzle";
+        model.addAttribute("genre", genre);   
+        model.addAttribute("isPC", true);
+        model.addAttribute("isConsole", false);
+        model.addAttribute("isMobile", true);
+        
         return "AddGameDetails";
     }
     
+    @GetMapping("/edit_game")
+    public String editGame(Model model) {
+        return "EditGameDetails";
+    }
+    
     @PostMapping("/add_game/submit")
-    public String addGameSubmit(Model model) {
+    public String addGameSubmit(Model model, @RequestParam String name, 
+            @RequestParam String details, @RequestParam String genreOptions,
+            @RequestParam String[] platformOptions) {
+        
+        String platformString = platformOptions[0];
+        for (int i = 1; i < platformOptions.length; i++) {
+            platformString += ", " + platformOptions[i];
+        }
+        
+        model.addAttribute("name", name);
+        model.addAttribute("details", details);
+        model.addAttribute("genre", genreOptions);
+        model.addAttribute("platforms", platformString);
         return "AddGameConfirmation";
     }
 }
--- src/main/java/com/gaff/demo/PlayerController.java
+++ src/main/java/com/gaff/demo/PlayerController.java
@@ -3,13 +3,12 @@ package com.gaff.demo;
 /*
  * This controller is for player-specific sections of the site.
  * Last updated 10/15/2022
- * Author(s): Alec Droegemeier
+ * Author(s): Alec Droegemeier, Jessica Frank
  */
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PlayerController {    
@@ -18,12 +17,12 @@ public class PlayerController {
         return "GameList";
     }
     
-    @PostMapping("/game_list/game")
+    @GetMapping("/game_details")
     public String addGameConfirm(Model model) {
         return "GameTemplate";
     }
     
-    @PostMapping("/player_profile")
+    @GetMapping("/player_profile")
     public String addGameSubmit(Model model) {
         return "Profile";
     }
