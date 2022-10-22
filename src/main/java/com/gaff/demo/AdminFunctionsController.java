562a4fa3f998603d1ae9dc76efc32454a424e940
Alex Wesley <a_wesley@uncg.edu>
2022-10-21 23:45:19 -0400

    I did good with admin pages

--- src/main/java/com/gaff/demo/AdminFunctionsController.java
+++ src/main/java/com/gaff/demo/AdminFunctionsController.java
@@ -9,6 +9,8 @@ package com.gaff.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminFunctionsController {
@@ -18,6 +20,13 @@ public class AdminFunctionsController {
     return "PromoteDemoteMod";
   }
   
+  @PostMapping("/change_role")
+  public String PromoteDemoteModMssg(Model model, @RequestParam String nameVar, @RequestParam String userType) {
+      model.addAttribute("again", true);
+      model.addAttribute("name", nameVar);
+      model.addAttribute("role", userType);
+    return "PromoteDemoteMod";
+  }
   @GetMapping("/view_logs")
   public String ViewSystemLogs (Model model) {
       return "ViewSystemLogs";
