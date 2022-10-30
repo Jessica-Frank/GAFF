79783f9d589e1bcf5b12d4438e67c2fe0c94a428
Alec <acdroegemei@uncg.edu>
2022-10-30 01:05:10 -0400

    updated gamelist to display games from MySQL

--- src/main/java/com/gaff/demo/PlayerController.java
+++ src/main/java/com/gaff/demo/PlayerController.java
@@ -6,6 +6,10 @@ package com.gaff.demo;
  * Author(s): Alec Droegemeier, Jessica Frank
  */
 import com.gaff.demo.models.AppUser;
+import com.gaff.demo.models.Game;
+import com.gaff.demo.models.GameRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -15,8 +19,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PlayerController {
 
+     @Autowired
+    GameRepository gameRep;
+     
     @GetMapping("/game_list")
     public String getGameList(Model model) {
+        List<Game> games = gameRep.getAllGames();
+        model.addAttribute("gameList", games);
         return "GameList";
     }
 
@@ -28,7 +37,49 @@ public class PlayerController {
         model.addAttribute("hasModRole", hasModRole);
         return "GameTemplate";
     }
-
+    
+    @GetMapping("/genre/action")
+    public String getActionGenre(Model model) {
+        List<Game> actionGames = gameRep.getActionGames();
+        model.addAttribute("gameList", actionGames);
+        return "GameList";
+    }
+        
+    @GetMapping("/genre/adventure")
+    public String getAdventureGenre(Model model) {
+        List<Game> adventureGames = gameRep.getAdventureGames();
+        model.addAttribute("gameList", adventureGames);
+        return "GameList";
+    }
+    
+    @GetMapping("/genre/simulation")
+    public String getSimulationGenre(Model model) {
+        List<Game> simulationGames = gameRep.getSimulationGames();
+        model.addAttribute("gameList", simulationGames);
+        return "GameList";
+    }
+    
+    @GetMapping("/genre/puzzle")
+    public String getPuzzleGenre(Model model) {
+        List<Game> puzzleGames = gameRep.getPuzzleGames();
+        model.addAttribute("gameList", puzzleGames);
+        return "GameList";
+    }
+    
+    @GetMapping("/genre/racing")
+    public String getRacingGenre(Model model) {
+        List<Game> racingGames = gameRep.getRacingGames();
+        model.addAttribute("gameList", racingGames);
+        return "GameList";
+    }
+    
+    @GetMapping("/genre/sports")
+    public String getSportsGenre(Model model) {
+        List<Game> sportsGames = gameRep.getSportsGames();
+        model.addAttribute("gameList", sportsGames);
+        return "GameList";
+    }
+    
     @GetMapping("/player_profile")
     public String playerProfile(Model model) {
         return "Profile";
--- src/main/java/com/gaff/demo/models/GameRepository.java
+++ src/main/java/com/gaff/demo/models/GameRepository.java
@@ -38,6 +38,107 @@ public class GameRepository {
                 )
         );
     }
+    
+    public List<Game> getActionGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Action'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
+    
+    public List<Game> getAdventureGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Adventure'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
+    public List<Game> getSimulationGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Simulation'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
+
+    public List<Game> getPuzzleGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Puzzle'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
+    
+    public List<Game> getRacingGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Racing'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
+    
+    public List<Game> getSportsGames() {
+        String query = "SELECT id, name, genre, description, "
+                + "is_computer, is_console, is_mobile FROM games Where genre='Sports'";
+        return template.query(query,
+                (result, rowNum)
+                -> new Game(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("genre"),
+                        result.getString("description"),
+                        result.getBoolean("is_computer"),
+                        result.getBoolean("is_console"),
+                        result.getBoolean("is_mobile")
+                )
+        );
+    }
 
     public Game getGameById(long id) {
         SqlParameterSource namedParameters
